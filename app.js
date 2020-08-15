dfebddc4100319bf53bcab364aa3b56d08b3d061
Angshuman Dutta <spamansh12@gmail.com>
2020-08-16 03:33:12 +0530

    a

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ var bodyParser = require("body-parser");
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
-app.use(express.static(__dirname + "/public"));
+express.static(public, [allow]);
+app.use(express.static('public'));
 
 app.get("/", function(req, res){
     res.render("index");
